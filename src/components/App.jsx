c4511761db329c12efcc04736ea82850317fd8b5
Gaultheria <valeriaburina@gmail.com>
2023-04-01 19:09:07 +0200

    step 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,16 +12,24 @@ export class App extends Component {
     {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
     ],
     name: '',
-    number: ''
+    number: '',
+    filter: '',
   };
   
   handleChange = evt => {
     const { name, value } = evt.target;
-    this.setState({
-      [name]: value,
-    })
+    
+    if (name === "filter") {
+      this.setState({
+        [name]: value.toLocaleLowerCase()
+      })
+    } else {
+      this.setState({
+        [name]: value,
+      })
+    }
   }
-  
+
   handleSubmit = evt => {
     evt.preventDefault();
     const { name, number } = this.state;
@@ -30,6 +38,7 @@ export class App extends Component {
       name: name,
       number: number
     }
+
     this.setState(prevState => ({
       contacts: [...prevState.contacts, newContact]
     }))
@@ -44,7 +53,10 @@ export class App extends Component {
   }
   
   render() {
-    const { contacts, name, number } = this.state;
+    const { contacts, name, number, filter } = this.state;
+    const filteredContacts = contacts.filter(contact =>
+      contact.name.toLocaleLowerCase().includes(filter)
+    );
 
     return (
       <div
@@ -53,7 +65,8 @@ export class App extends Component {
           display: 'flex',
           flexDirection: 'column',
           justifyContent: 'center',
-          alignItems: 'center',
+          //alignItems: 'center',
+          marginLeft: '70vh',
           fontSize: 20,
           color: '#010101'
         }}
@@ -71,7 +84,7 @@ export class App extends Component {
               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
               id='nameInput' required />
           </label> 
-          <label htmlFor='nameInput'> <h3>Phone number</h3>
+          <label htmlFor='nameInput'> <h3>Number</h3>
             <input
               type="tel"
               name="number"
@@ -81,12 +94,25 @@ export class App extends Component {
               title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
               id='nameInput' required />
           </label>
-          <button type='submit' onSubmit={this.handleSubmit}>Add contact</button>
+          <br /><br />
+          <button style={{
+            fontSize: '20px'
+           }} type='submit' onSubmit={this.handleSubmit}>Add contact</button>
         </form>
         
-        <h2>Contacts</h2>
+        <h2 style={{ margin: '0px', marginTop: '50px' }}>Contacts</h2>
+        <form>
+          <label htmlFor='filterInput'> <h3>Find contacts by name</h3>
+            <input
+              type="text"
+              name="filter"
+              value={filter}
+              onChange = {this.handleChange}
+              id='filterInput' required />
+          </label>
+        </form>
         <ul>
-          {contacts.map(contact => (
+          {filteredContacts.map(contact => (
             <li key={contact.id} name={contact.name} > {contact.name}{' '}{contact.number} </li>
           ))}
         </ul>
